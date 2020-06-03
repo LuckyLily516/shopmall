4d1f44b5ab53485572ddad307156a129d2478912
ickt_20_孙霄丽 <2450727615@qq.com>
2020-06-03 23:01:03 +0800

    tabbar组件封装

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="item">
+      <div><slot name='image'></slot></div>
+      <div><slot name='text'></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.item {
+    height: 49px;
+}
+
+</style>
---
+++ src/components/content/maintabbar/MainTabBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <tab-bar>
+          <tab-bar-item>
+              <!-- <img slot="image" src="" alt="">
+              <span slot="text" content="首页"></span> -->
+          </tab-bar-item>
+          <tab-bar-item></tab-bar-item>
+          <tab-bar-item></tab-bar-item>
+          <tab-bar-item></tab-bar-item>
+      </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBarItem from '../../common/tabbar/TabBarItem'
+import TabBar from '../../common/tabbar/TabBar'
+
+export default {
+    components: {
+        TabBarItem,
+        TabBar
+    }
+
+}
+</script>
+
+<style>
+
+</style>
